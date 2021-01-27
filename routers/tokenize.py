8929f8432470bb6dba706d90f60b6484bb6bd15f
ppirch <pakin.pirch@gmail.com>
2021-01-28 04:04:29 +0700

    Add TokenizeEngine Class , WordTokenizeResponse Class and Restructuring

--- routers/tokenize.py
+++ routers/tokenize.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 from pythainlp import tokenize
 from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel
 
 router = APIRouter()
+
 
 class TokenizeEngine(str, Enum):
     newmm = "newmm"
@@ -11,6 +14,11 @@ class TokenizeEngine(str, Enum):
     icu = "icu"
     ulmfit = "ulmfit"
 
-@router.get('/word_tokenize')
+
+class WordTokenizeResponse(BaseModel):
+    words: List[str] = []
+
+
+@router.get('/word_tokenize', response_model=WordTokenizeResponse)
 def word_tokenize(q: str, engine: TokenizeEngine = "newmm"):
-    return '|'.join(tokenize.word_tokenize(q,engine=engine))
+    return {"words": tokenize.word_tokenize(q, engine=engine)}
